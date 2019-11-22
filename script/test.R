76c74ad51cc4333a0efafb31d11e8d9ce2f050c9
RafaMariano <rafaelmonteiromariano@gmail.com>
2019-11-22 17:25:01 -0300

    creating vignettes - add author information

--- script/test.R
+++ script/test.R
@@ -1,7 +1,7 @@
 library(sits.rep)
 
 sits.rep::sits.rep_classify("arv_1", "~/workspace/sits.rep/script/classification.R")
-
+sits.rep::sits.rep_reproduce("arv_1", "classification", "rep_deeplearnings")
 
 
 # sits.rep::sits.rep_pos_processing("arv_1/classification",
